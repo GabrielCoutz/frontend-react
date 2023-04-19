58359dc67f3c81406d8195b726be39d9705c393c
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-19 16:33:14 -0300

    feat(button component): add disabled style

--- src/components/Button/Primary.tsx
+++ src/components/Button/Primary.tsx
@@ -5,7 +5,7 @@ export const Primary = (props: ButtonProps) => {
   return (
     <button
       {...props}
-      className={`font-semibold bg-indigo-600 hover:bg-indigo-700 py-2 px-8 text-slate-50 transition border-transparent border h-min-9 rounded-lg ${props.className}`}
+      className={`font-semibold bg-indigo-600 hover:bg-indigo-700 py-2 px-8 text-slate-50 transition border-transparent border h-min-9 rounded-lg disabled:bg-indigo-400 disabled:cursor-not-allowed ${props.className}`}
     >
       {props.children}
     </button>
