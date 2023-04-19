fa8202214c7bff51392bdd0ee97ccc3bb060f906
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-19 16:33:37 -0300

    feat(signin form): sign form methods complete

--- src/components/SigninForm/index.tsx
+++ src/components/SigninForm/index.tsx
@@ -1,74 +1,84 @@
-import React from 'react'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { api } from '../../helpers/request'
+import { ApiErrorResponse } from '../../helpers/request/error'
+
+import { Button } from '../Button'
+import { Form } from '../Form'
+
+interface SigninFormSchema {
+  email: string
+  password: string
+}
 
 const SigninForm = () => {
+  const signinFormMethods = useForm<SigninFormSchema>()
+  const { handleSubmit } = signinFormMethods
+  const [error, setError] = useState<string | undefined>(undefined)
+  const [loading, setLoading] = useState<boolean | undefined>(undefined)
+
+  const handleSignin = (payload: SigninFormSchema) => {
+    setError(undefined)
+    setLoading(true)
+
+    api.auth
+      .login(payload)
+      .then(({ data }) => console.log(data))
+      .catch(({ response }: ApiErrorResponse) => {
+        if (response?.data.statusCode == 401)
+          return setError('Credenciais inválidas')
+        else setError('Erro inesperado, por vafor tente novamente mais tarde.')
+        console.log(response?.data)
+      })
+      .finally(() => setLoading(false))
+  }
+
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Entre na sua conta
-        </h2>
-      </div>
+    <div className="mx-auto p-4 max-w-sm sm:w-full">
+      <FormProvider {...signinFormMethods}>
+        <form
+          onSubmit={handleSubmit(handleSignin)}
+          className="mx-auto rounded flex flex-col gap-4"
+        >
+          <Form.Field>
+            <Form.Label htmlFor="email">Email</Form.Label>
+            <Form.Input
+              name="email"
+              type="email"
+              errormessage="Preencha este campo"
+              validation={{ value: /\S+@\S+\.\S+/, message: 'Email inválido' }}
+            />
+            <Form.Error field="email" />
+          </Form.Field>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6">
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Email
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
+          <Form.Field>
+            <Form.Label htmlFor="password">Senha</Form.Label>
+            <Form.Input
+              name="password"
+              type="password"
+              errormessage="Preencha este campo"
+            />
+            <Form.Error field="password" />
+          </Form.Field>
 
-          <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Senha
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Entrar
-            </button>
-          </div>
+          {error && (
+            <span className="text-red-500 font-medium text-center text-sm">
+              {error}
+            </span>
+          )}
+          <Button.Primary className="mt-2" disabled={loading}>
+            {loading ? 'Carregando...' : 'Entrar'}
+          </Button.Primary>
         </form>
 
         <p className="mt-10 text-center text-sm text-gray-500">
           Não tem conta?{' '}
-          <a
-            href="#"
-            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >
-            Faça uma agora!
-          </a>
+          <Link href="signup">
+            <Button.Terciary>Faça uma agora!</Button.Terciary>
+          </Link>
         </p>
-      </div>
+      </FormProvider>
     </div>
   )
 }
