705f89a9e15c80093b35c638bb77fbff7230bbce
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-19 14:49:02 -0300

    feat(home): home page complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,19 @@ import { queryClient } from '../services/queryClient'
 export default function Home() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>xampson</h1>
-      <Link href="signup">
-        <Button.Primary>Cadastro</Button.Primary>
-      </Link>
-      <Link href="signup">
-        <Button.Secondary>Login</Button.Secondary>
-      </Link>
-      <Link href="signup">
-        <Button.Terciary>xampson</Button.Terciary>
-      </Link>
+      <section className="flex align-middle p-8 mt-32 max-md:mt-16">
+        <div className="mx-auto">
+          <h1 className="text-3xl max-md:text-2xl tracking-wide font-bold text-slate-950 mb-8 max-md:mb-4">
+            Cadastre-se ou faça login
+          </h1>
+          <Link href="signup" className="block w-full">
+            <Button.Primary className="w-full">Cadastro</Button.Primary>
+          </Link>
+          <Link href="signin" className="block w-full mt-4">
+            <Button.Secondary className="w-full">Login</Button.Secondary>
+          </Link>
+        </div>
+      </section>
     </QueryClientProvider>
   )
 }
