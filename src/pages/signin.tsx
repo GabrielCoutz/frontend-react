a0b43960f3b4da0c1a0b21a2ace59d3e8d918718
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-19 15:35:28 -0300

    feat(signin form): page to signin complete

---
+++ src/pages/signin.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SigninForm from '../components/SigninForm'
+
+const signin = () => {
+  return (
+    <section>
+      <h1 className="text-center text-2xl text-slate-800 font-semibold mt-48 max-md:mt-24">
+        É bom te ver de novo
+      </h1>
+      <SigninForm />
+    </section>
+  )
+}
+
+export default signin
