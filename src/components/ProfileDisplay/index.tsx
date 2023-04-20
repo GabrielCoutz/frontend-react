b1aa778e1a58813cae81774fb3d7f23efeb2c324
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-20 16:34:36 -0300

    feat(profile): created display component to show components from options in profile

---
+++ src/components/ProfileDisplay/index.tsx
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react'
+import { ProfileContext } from '../../context/profile'
+
+export const ProfileDisplay = () => {
+  const { activeOption } = useContext(ProfileContext)
+  return (
+    <div className="shadow-md rounded-lg rounded-s-none p-4 max-md:rounded-t-none max-md:rounded-b-lg">
+      {activeOption?.component}
+    </div>
+  )
+}
