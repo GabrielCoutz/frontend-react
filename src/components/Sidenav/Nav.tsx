bce3ccbffc6b82e996d7e5e7cc0ecdd00e2d4902
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-20 16:33:44 -0300

    feat(nav): add shadow and remove padding

--- src/components/Sidenav/Nav.tsx
+++ src/components/Sidenav/Nav.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from 'react'
 
 export const Nav = ({ children }: PropsWithChildren) => {
   return (
-    <div className="bg-indigo-700 md:p-4 rounded-lg rounded-e-none max-md:rounded-b-none max-md:rounded-t-lg">
+    <div className="shadow-md bg-indigo-700 rounded-lg rounded-e-none max-md:rounded-b-none max-md:rounded-t-lg">
       <nav>{children}</nav>
     </div>
   )
