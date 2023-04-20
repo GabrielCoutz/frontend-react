0ef5c0cce70492d27439e26c1e9d07545da4f139
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-20 08:56:36 -0300

    feat(interfaces/user): update user interface

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,4 +1,11 @@
+import { IProductList } from "./Product"
+
 export interface IUser {
   id: string
   name: string
+  email: string
+  created_at: string
+  products: IProductList
 }
+
+export type IUserList = IUser[]
