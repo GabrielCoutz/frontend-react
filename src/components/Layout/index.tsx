a7cfc257acec28cbd986d048e72b33c75fe7609b
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-21 00:14:39 -0300

    fix(contexts/header): fixed scope of  header context

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,18 +6,20 @@ const Layout = ({ children }: PropsWithChildren) => {
   const [menuIsOpen, setMenuIsOpen] = useState(false)
 
   return (
-    <HeaderContext.Provider value={{ menuIsOpen, setMenuIsOpen }}>
-      <Header.Background>
-        <Header.Container>
-          <Header.Logo />
-          <Header.MobileButton />
-          <Header.Nav>
-            <Header.Links />
-          </Header.Nav>
-        </Header.Container>
-      </Header.Background>
+    <>
+      <HeaderContext.Provider value={{ menuIsOpen, setMenuIsOpen }}>
+        <Header.Background>
+          <Header.Container>
+            <Header.Logo />
+            <Header.MobileButton />
+            <Header.Nav>
+              <Header.Links />
+            </Header.Nav>
+          </Header.Container>
+        </Header.Background>
+      </HeaderContext.Provider>
       {children}
-    </HeaderContext.Provider>
+    </>
   )
 }
 
