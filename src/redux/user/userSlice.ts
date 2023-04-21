2fa4d446e48f0946e547eb8043218d516dd913f2
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-21 12:42:41 -0300

    feat(store/user): add update user method

--- src/redux/user/userSlice.ts
+++ src/redux/user/userSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { UpdateUserPayload } from '../../helpers/request/user'
 import { IUser } from '../../interfaces/User'
 
 const initialState: IUser = {} as IUser
@@ -11,8 +12,12 @@ export const userSlice = createSlice({
       ...state,
       ...payload,
     }),
+    updateUser: (state, { payload }: PayloadAction<UpdateUserPayload>) => ({
+      ...state,
+      ...payload,
+    }),
   },
 })
 
-export const { saveUser } = userSlice.actions
+export const { saveUser, updateUser } = userSlice.actions
 export default userSlice.reducer
