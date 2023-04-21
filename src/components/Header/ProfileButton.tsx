14aad8b560f403528cfa7afedb0b0886028ee570
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-21 12:42:06 -0300

    feat(header): user name capitalized

--- src/components/Header/ProfileButton.tsx
+++ src/components/Header/ProfileButton.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { capitalize } from '../../helpers/capitalize'
 import { selectUserName } from '../../redux/user/userSelectors'
 import { HeaderLink } from './HeaderLink'
 
 export const ProfileButton = () => {
   const userName = selectUserName()
+  const firstName = userName.split(' ')[0]
 
-  return <HeaderLink href="profile">{userName}</HeaderLink>
+  return <HeaderLink href="profile">{capitalize(firstName)}</HeaderLink>
 }
