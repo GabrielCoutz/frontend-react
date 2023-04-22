7b30cf85d7881562be149f1ccbe891b7d8737ec6
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-22 00:02:36 -0300

    refactor(button): update buttonprops interface

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,14 +1,18 @@
-import { ButtonHTMLAttributes } from 'react'
+import {
+  ButtonHTMLAttributes,
+  DetailedHTMLProps,
+  PropsWithChildren,
+} from 'react'
 import { Primary } from './Primary'
 import { Secondary } from './Secondary'
 import { Terciary } from './Terciary'
 
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children: string
-}
+export type ButtonProps = PropsWithChildren<
+  DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+>
 
 export const Button = {
   Primary,
   Secondary,
-  Terciary
+  Terciary,
 }
