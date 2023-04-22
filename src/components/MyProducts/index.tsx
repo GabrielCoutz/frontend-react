78bafa79a284b84bed243e0d3930496737f4f73f
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-22 09:35:55 -0300

    feat(components/myproducts): editProduct component implemented

--- src/components/MyProducts/index.tsx
+++ src/components/MyProducts/index.tsx
@@ -1,5 +1,36 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+import { selectUserProducts } from '../../redux/user/userSelectors'
+import { Button } from '../Button'
+import { Product } from '../Product'
+import { EditProduct } from '../EditProduct'
 
 export const MyProducts = () => {
-  return <div>my products</div>
+  const products = useSelector(selectUserProducts)
+
+  return (
+    <Product.List className="col-span-full">
+      {products.map((product) => (
+        <Product.Card key={product.id} className="grid grid-cols-2">
+          <EditProduct.Wrapper>
+            <EditProduct.Header>
+              <div>
+                <Product.Title>{product.name}</Product.Title>
+                <Product.Price>{product.price}</Product.Price>
+              </div>
+            </EditProduct.Header>
+            <EditProduct.Toggle
+              as="div"
+              className="col-start-2 self-start justify-self-end"
+            >
+              <Button.Terciary>Editar</Button.Terciary>
+            </EditProduct.Toggle>
+
+            <EditProduct.Content>tudo mais</EditProduct.Content>
+          </EditProduct.Wrapper>
+        </Product.Card>
+      ))}
+    </Product.List>
+  )
 }
