6dd7cb0db7d8a8f2364688afe5fcc9452ef62e9d
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-22 09:36:22 -0300

    feat(redux/user): created select for user products

--- src/redux/user/userSelectors.ts
+++ src/redux/user/userSelectors.ts
@@ -5,3 +5,5 @@ export const selectUserState = ({ user }: RootState) => user
 export const selectUserName = ({ user }: RootState) => user?.user?.name
 
 export const selectUser = ({ user }: RootState) => user?.user
+
+export const selectUserProducts = ({ user }: RootState) => user.user.products
