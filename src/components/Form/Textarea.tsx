3450b8de9a70ff3e41aa9c5a661aeccd61dceca2
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-23 19:04:33 -0300

    feat(form): add error message in textare

--- src/components/Form/Textarea.tsx
+++ src/components/Form/Textarea.tsx
@@ -3,6 +3,7 @@ import { useFormContext } from 'react-hook-form'
 
 interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   name: string
+  errormessage?: string
 }
 
 export const Textarea = (props: TextareaProps) => {
@@ -12,7 +13,7 @@ export const Textarea = (props: TextareaProps) => {
     <textarea
       {...props}
       id={props.name}
-      {...register(props.name)}
+      {...register(props.name, { required: props.errormessage })}
       className={`rounded-lg border-gray-200 shadow hover:shadow-sm focus:ring-inset-indigo-600 ring-inset transition text-sm focus:ring-indigo-700 h-auto ${props.className}`}
     ></textarea>
   )
