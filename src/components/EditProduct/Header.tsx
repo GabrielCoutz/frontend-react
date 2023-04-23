aed4e5bbd9717698bde645044cbcd0666ae2c938
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-23 19:05:20 -0300

    refactor(accordion): deleted components replaced by Accordion component

--- src/components/EditProduct/Header.tsx
+++
@@ -1,7 +0,0 @@
-import React, { HtmlHTMLAttributes, PropsWithChildren } from 'react'
-
-export const Header = (
-  props: PropsWithChildren<HtmlHTMLAttributes<HTMLDivElement>>,
-) => {
-  return <div {...props}>{props.children}</div>
-}
--- src/components/EditProduct/Toggle.tsx
+++
@@ -1,8 +0,0 @@
-import { Disclosure, DisclosureButtonProps } from '@headlessui/react'
-import React, { ElementType, PropsWithChildren } from 'react'
-
-export const Toggle = (
-  props: PropsWithChildren<DisclosureButtonProps<ElementType>>,
-) => {
-  return <Disclosure.Button {...props}>{props.children}</Disclosure.Button>
-}
