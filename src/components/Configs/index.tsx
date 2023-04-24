d86b86fe82ee9aab43ad55e6d9c5df8bb4faf313
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-24 13:14:25 -0300

    feat(components/config): dangerzone component implemented

--- src/components/Configs/index.tsx
+++ src/components/Configs/index.tsx
@@ -1,5 +1,34 @@
 import React from 'react'
+import { Dangerzone } from '../Dangerzone'
+import { DeleteAccountForm } from '../DeleteAccountForm'
 
 export const Config = () => {
-  return <div>config</div>
+  return (
+    <Dangerzone.Wrapper>
+      <Dangerzone.Header>
+        <span className="text-slate-800 font-medium text-lg">
+          Ações destrutivas
+        </span>
+        <p className="text-slate-600">
+          As opções abaixo são{' '}
+          <strong className="text-red-500">IRREVERSÍVEIS</strong>. Por favor,
+          cautela ao proseguir.
+        </p>
+      </Dangerzone.Header>
+
+      <Dangerzone.Row>
+        <Dangerzone.Description>
+          <span className="text-slate-800 font-medium">Deletar conta</span>
+          <p className="text-slate-600">
+            Ao deletar sua conta, todos os dados também são deletados. Não é
+            possível reverter esta ação.
+          </p>
+        </Dangerzone.Description>
+
+        <Dangerzone.Action>
+          <DeleteAccountForm />
+        </Dangerzone.Action>
+      </Dangerzone.Row>
+    </Dangerzone.Wrapper>
+  )
 }
