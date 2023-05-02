e425f72ea21b0c40440b9ba8b6726cb980f7b0a4
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-02 19:21:26 -0300

    refactor(form/textarea): created controled props

--- src/components/Form/Textarea/index.tsx
+++ src/components/Form/Textarea/index.tsx
@@ -1,21 +1,33 @@
-import React, { TextareaHTMLAttributes } from 'react'
+import React from 'react'
 import { useFormContext } from 'react-hook-form'
 
-interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+interface TextareaProps {
   name: string
   errormessage?: string
+  cols?: number
+  rows?: number
+  placeholder?: string
+  error?: boolean
 }
 
-export const Textarea = (props: TextareaProps) => {
-  const { register } = useFormContext()
+export const Textarea = ({ name, errormessage, cols, rows, placeholder, error }: TextareaProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext()
+  const fieldErro = errors[name]?.message?.toString()
 
   return (
     <textarea
-      data-testid="textarea"
-      {...props}
-      id={props.name}
-      {...register(props.name, { required: props.errormessage })}
-      className={`rounded-lg border-gray-200 shadow hover:shadow-sm focus:ring-inset-primary-600 ring-inset transition text-sm focus:ring-primary-700 h-auto ${props.className}`}
+      data-testid="form-textarea"
+      id={name}
+      cols={cols}
+      rows={rows}
+      placeholder={placeholder}
+      {...register(name, { required: errormessage })}
+      className={`rounded-lg border-gray-200 shadow hover:shadow-sm focus:ring-inset-primary-600 ring-inset transition text-sm focus:ring-primary-700 h-auto ${
+        fieldErro || error ? 'ring-2 ring-red-500' : ''
+      }`}
     ></textarea>
   )
 }
