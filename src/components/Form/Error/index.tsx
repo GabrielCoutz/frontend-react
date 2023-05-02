09c91eb4993954d60a5048624edec581b9c8aafb
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-02 11:53:24 -0300

    refactor(components/form/error): increase reability

--- src/components/Form/Error/index.tsx
+++ src/components/Form/Error/index.tsx
@@ -10,14 +10,15 @@ export const Error = ({ field }: ErrorProps) => {
   const {
     formState: { errors },
   } = useFormContext()
+  const erroMessage = errors[field]?.message?.toString()
 
-  return errors[field]?.message?.toString() ? (
+  return erroMessage ? (
     <span
       className="text-red-500 flex text-sm items-center"
       data-testid="form-error"
     >
       <ExclamationCircleIcon className="h-4 w-4 text-red-600 me-1" />
-      {errors[field]?.message?.toString()}
+      {erroMessage}
     </span>
   ) : null
 }
