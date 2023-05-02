f4ba42215d2ff111964f85f14fa392b5afcb3720
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-02 19:22:32 -0300

    feat(modal/actions): add style to button in actions

--- src/components/Modal/Actions/index.tsx
+++ src/components/Modal/Actions/index.tsx
@@ -5,5 +5,12 @@ interface ActionsProps {
 }
 
 export const Actions = ({ children }: ActionsProps) => {
-  return <div data-testid="modal-actions">{children}</div>
+  return (
+    <div
+      data-testid="modal-actions"
+      className="flex gap-2 justify-center max-md:flex-col"
+    >
+      {children}
+    </div>
+  )
 }
