c99f1d347f07cba3affc3c20104da983a888dba0
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-04 11:24:29 -0300

    feat(components/myproducts): add button to announce product

--- src/components/MyProducts/Products/index.tsx
+++ src/components/MyProducts/Products/index.tsx
@@ -1,39 +1,63 @@
 import { useSelector } from 'react-redux'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 import { selectUserProducts } from '../../../redux/product/productSelectors'
 import { EditProduct } from '../../EditProduct'
 import { Accordion } from '../../Accordion'
 import { Product } from '../../Product'
 import { Button } from '../../Button'
+import { CreateProductForm } from '../../CreateProductForm'
 
 export const Products = () => {
+  const [accordionIsOpen, setAccordionIsOpen] = useState(false)
   const products = useSelector(selectUserProducts)
 
   return (
-    <Product.List className="col-span-full" data-testid="myproducts-products">
-      {products?.map((product) => (
-        <Product.Card key={product.id} className="grid grid-cols-2">
-          <Accordion.Wrapper className="grid grid-cols-2 col-span-full gap-y-4">
-            <Accordion.Header>
-              <Product.Title>{product.name}</Product.Title>
-              <Product.Price>{product.price}</Product.Price>
-            </Accordion.Header>
+    <>
+      <Accordion.Wrapper>
+        <Accordion.Header></Accordion.Header>
+        <Accordion.Toggle
+          onClick={() => setAccordionIsOpen(!accordionIsOpen)}
+          className="flex justify-center"
+        >
+          {accordionIsOpen ? (
+            <Button.Secondary>Cancelar</Button.Secondary>
+          ) : (
+            <Button.Primary>Anunciar produto</Button.Primary>
+          )}
+        </Accordion.Toggle>
 
-            <Accordion.Toggle className="col-start-2 self-start justify-self-end">
-              <Button.Terciary>Editar</Button.Terciary>
-            </Accordion.Toggle>
-            <Link href={`/product/${product.id}`}>
-              <Button.Terciary>Ver produto</Button.Terciary>
-            </Link>
+        <Accordion.Content>
+          <CreateProductForm />
+        </Accordion.Content>
+      </Accordion.Wrapper>
+      <Product.List
+        className="col-span-full border-t-2 border-slate-100 pt-4"
+        data-testid="myproducts-products"
+      >
+        {products?.map((product) => (
+          <Product.Card key={product.id} className="grid grid-cols-2">
+            <Accordion.Wrapper className="grid grid-cols-2 col-span-full gap-y-4">
+              <Accordion.Header>
+                <Product.Title>{product.name}</Product.Title>
+                <Product.Price>{product.price}</Product.Price>
+              </Accordion.Header>
 
-            <Accordion.Content className="col-span-full">
-              <EditProduct.EditForm product={product} />
-            </Accordion.Content>
-          </Accordion.Wrapper>
-        </Product.Card>
-      ))}
-    </Product.List>
+              <Accordion.Toggle className="col-start-2 self-start justify-self-end">
+                <Button.Terciary>Editar</Button.Terciary>
+              </Accordion.Toggle>
+              <Link href={`/product/${product.id}`}>
+                <Button.Terciary>Ver produto</Button.Terciary>
+              </Link>
+
+              <Accordion.Content className="col-span-full">
+                <EditProduct.EditForm product={product} />
+              </Accordion.Content>
+            </Accordion.Wrapper>
+          </Product.Card>
+        ))}
+      </Product.List>
+    </>
   )
 }
