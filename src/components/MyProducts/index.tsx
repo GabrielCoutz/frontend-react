e751ce53b7a88d4b8b59c90a010e2bdab94476a7
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-06 14:21:21 -0300

    feat(components): removed unecessary modal files & imports

--- src/components/MyProducts/index.tsx
+++ src/components/MyProducts/index.tsx
@@ -4,16 +4,10 @@ import { useSelector } from 'react-redux'
 import { selectUserProducts } from '../../redux/product/productSelectors'
 import { Products } from './Products'
 import { Empty } from './Empty'
-import { ProductsEventListener } from './ProductsEventListener'
 
 export const MyProducts = () => {
   const products = useSelector(selectUserProducts)
   const userHasProducts = products?.length
 
-  return (
-    <>
-      <ProductsEventListener />
-      {userHasProducts ? <Products /> : <Empty />}
-    </>
-  )
+  return userHasProducts ? <Products /> : <Empty />
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useState } from 'react'
 import { Provider } from 'react-redux'
 
 import Layout from '../components/Layout'
 import store from '../redux/store'
-import { IModalTrigger, ModalContext, openModal } from '../contexts/modal'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [trigger, setTrigger] = useState<IModalTrigger>(undefined)
-
   return (
     <Provider store={store}>
-      <ModalContext.Provider value={{ openModal, trigger, setTrigger }}>
-        <Layout>
-          <Component {...pageProps} />
-          {openModal(trigger)}
-        </Layout>
-      </ModalContext.Provider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </Provider>
   )
 }
