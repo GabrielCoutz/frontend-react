19994f05c2b388532818e65ca51f355c4693f89f
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-16 16:31:44 -0300

    refactor(components/modal): removed components from headlessui, using only tailwind instead

--- src/components/Modal/Wrapper/index.tsx
+++ src/components/Modal/Wrapper/index.tsx
@@ -1,10 +1,50 @@
-import React, { PropsWithChildren } from 'react'
+'use client'
 
-export const Wrapper = ({ children }: PropsWithChildren) => {
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import { useRouter } from 'next/navigation'
+import React, { MouseEvent, ReactNode, useRef } from 'react'
+
+interface WrapperProps {
+  children: ReactNode
+  className?: string
+}
+
+export const Wrapper = ({ children, className }: WrapperProps) => {
+  const backgroundRef = useRef<null | HTMLDivElement>(null)
+  const { back } = useRouter()
+
+  const handleOutsideClick = (e: MouseEvent<HTMLDivElement>) => {
+    const clickedElement = e.target
+    const backgroundWasClicked = clickedElement === backgroundRef.current
+
+    if (backgroundWasClicked) back()
+  }
+
   return (
-    <div className="fixed inset-0 z-10 overflow-y-auto">
-      <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-        {children}
+    <div
+      className="fixed top-0 inset-x-0 overflow-hidden z-10"
+      onClick={handleOutsideClick}
+      data-testid="modal-wrapper"
+    >
+      <div
+        className="flex items-center justify-center h-screen bg-gray-500/75 backdrop-blur p-16"
+        ref={backgroundRef}
+      >
+        <div
+          className={`z-10 bg-white rounded shadow-lg p-4 max-w-xs w-full text-center relative ${className}`}
+        >
+          <div className="absolute top-0 right-0">
+            <button
+              onClick={back}
+              className="p-2"
+              type="button"
+              aria-label="fechar modal"
+            >
+              <XMarkIcon className="w-6 h-6 text-slate-600" />
+            </button>
+          </div>
+          {children}
+        </div>
       </div>
     </div>
   )
--- src/components/Modal/index.ts
+++ src/components/Modal/index.ts
@@ -1,15 +1,13 @@
-import { Trigger } from './Trigger'
-import { Body } from './Body'
 import { Title } from './Title'
+import { Wrapper } from './Wrapper'
 import { Message } from './Message'
 import { Actions } from './Actions'
 import { IconWrapper } from './IconWrapper'
 
 export const Modal = {
-  Trigger,
-  Body,
   Title,
   Message,
   Actions,
+  Wrapper,
   IconWrapper,
 }
