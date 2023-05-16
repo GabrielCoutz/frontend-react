93df34cb34a653d89bff4d840d0cfa878c657002
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-16 14:47:08 -0300

    feat: removed no more necessary files

--- src/contexts/header/index.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-
-interface IHeaderContext {
-  menuIsOpen: boolean
-  setMenuIsOpen: React.Dispatch<React.SetStateAction<boolean>>
-}
-
-export const HeaderContext = createContext<IHeaderContext>({} as IHeaderContext)
