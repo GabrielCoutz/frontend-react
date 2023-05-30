e22059ea8cd565d19c4f52a73eb03ec1ab6e360b
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-30 18:23:43 -0300

    feat(modal/wrapper): add option to call when modal is closed

--- src/components/Modal/Wrapper/index.tsx
+++ src/components/Modal/Wrapper/index.tsx
@@ -7,17 +7,19 @@ import React, { MouseEvent, ReactNode, useRef } from 'react'
 interface WrapperProps {
   children: ReactNode
   className?: string
+  onClick?: () => void
 }
 
-export const Wrapper = ({ children, className }: WrapperProps) => {
+export const Wrapper = ({ children, className, onClick }: WrapperProps) => {
   const backgroundRef = useRef<null | HTMLDivElement>(null)
   const { back } = useRouter()
+  const closeCallback = onClick || back
 
   const handleOutsideClick = (e: MouseEvent<HTMLDivElement>) => {
     const clickedElement = e.target
     const backgroundWasClicked = clickedElement === backgroundRef.current
 
-    if (backgroundWasClicked) back()
+    if (backgroundWasClicked) closeCallback()
   }
 
   return (
@@ -28,17 +30,19 @@ export const Wrapper = ({ children, className }: WrapperProps) => {
     >
       <div
         className="flex items-center justify-center h-screen bg-gray-500/75 backdrop-blur p-16"
+        data-testid="modal-background"
         ref={backgroundRef}
       >
         <div
-          className={`z-10 bg-white rounded shadow-lg p-4 max-w-xs w-full text-center relative ${className}`}
+          className={`z-10 bg-white rounded shadow-lg p-4 max-w-sm w-full text-center relative ${className}`}
         >
           <div className="absolute top-0 right-0">
             <button
-              onClick={back}
+              onClick={closeCallback}
               className="p-2"
               type="button"
               aria-label="fechar modal"
+              data-testid="modal-close-button"
             >
               <XMarkIcon className="w-6 h-6 text-slate-600" />
             </button>
